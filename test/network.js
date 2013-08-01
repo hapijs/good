cd00be55b4152a39931bb0f1e98edc9a93d076ec
Wyatt Preul <wpreul@gmail.com>
2013-08-01 09:48:45 -0500

    Adding test

--- test/network.js
+++ test/network.js
@@ -71,4 +71,35 @@ describe('Network Monitor', function () {
 
         done();
     });
+
+    it('tracks requests by server', function (done) {
+
+        var emitter = new Events.EventEmitter();
+        var network = new NetworkMonitor.Monitor(emitter);
+
+        var tags = ['hapi', 'received'];
+        var tagsMap = Hoek.mapToObject(tags);
+        var request1 = { server: { info: { port: 80 }}}
+        var request2 = { server: { info: { port: 443 }}}
+        emitter.emit('request', request1, { tags: tags }, tagsMap);
+        emitter.emit('request', request1, { tags: tags }, tagsMap);
+        emitter.emit('response', request1);
+        emitter.emit('request', request2, { tags: tags }, tagsMap);
+        emitter.emit('request', request2, { tags: tags }, tagsMap);
+        emitter.emit('request', request2, { tags: tags }, tagsMap);
+
+        network.requests(function (err, result) {
+
+            expect(result['80']).to.equal(2);
+            expect(result['443']).to.equal(3);
+        });
+
+        network.concurrents(function (err, result) {
+
+            expect(result['80']).to.equal(1);
+            expect(result['443']).to.equal(3);
+        });
+
+        done();
+    });
 });
