c9821cb88085ca1d2f35bb7ebbb38bfb4a518248
lloydbenson <lloyd.benson@gmail.com>
2014-08-08 06:54:52 -0500

    support new lab

--- test/replay.js
+++ test/replay.js
@@ -15,11 +15,13 @@ var internals = {};
 
 // Test shortcuts
 
+var lab = exports.lab = Lab.script();
 var expect = Lab.expect;
-var before = Lab.before;
-var after = Lab.after;
-var describe = Lab.experiment;
-var it = Lab.test;
+var before = lab.before;
+var after = lab.after;
+var describe = lab.describe;
+var it = lab.it;
+
 
 describe('Replay', function () {
 
