3933e4e91b7caeef7d0a30afe24a2f76d4557258
Wyatt Preul <wpreul@gmail.com>
2015-03-31 12:22:59 -0500

    More tests

--- test/utils.js
+++ test/utils.js
@@ -2,7 +2,6 @@
 
 var Code = require('code');
 var Lab = require('lab');
-
 var Utils = require('../lib/utils');
 
 
@@ -35,6 +34,26 @@ describe('utils', function () {
                 expect(value).to.be.true();
                 done();
             });
+        });
+    });
+
+    describe('GreatWreck()', function () {
+
+        it('handles a null request and response', function (done) {
+
+            var greatWreck = new Utils.GreatWreck();
+            expect(greatWreck.request).to.exist();
+            expect(greatWreck.response).to.exist();
+            done();
+        });
+
+        it('reports on errors', function (done) {
+
+            var error = new Error('my error');
+            var greatWreck = new Utils.GreatWreck(error);
+
+            expect(greatWreck.error.message).to.equal('my error');
+            done();
         });
     });
 });
