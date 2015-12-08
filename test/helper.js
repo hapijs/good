d8e04fc372ee4723fac8f70ec65985bec89fde88
Adam Bretz <arbretz@gmail.com>
2015-12-08 06:26:49 -0500

    Closes #413. Push null to the data stream for EOF. Closes #415. Remove "stop" event.

--- test/helper.js
+++ test/helper.js
@@ -10,21 +10,18 @@ class Reporter {
         this.handler = datahandler || function () {};
     }
     init(stream, emitter, callback) {
-
         stream.on('data', (data) => {
-
             if (this.events[data.event]) {
                 this.messages.push(data);
                 this.handler(data);
             }
         });
 
-        emitter.once('stop', () => {
-
+        stream.on('end', () => {
             this.stopped = true;
         });
 
-        callback();
+        process.nextTick(callback);
     }
 }
 
